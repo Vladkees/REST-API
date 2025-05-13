fe3f932eec93b8899e5bd50953feca19f1a418d7
Vlad Prokutko <147252749+Vladkees@users.noreply.github.com>
2025-05-13 20:48:34 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,4 +3,3 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:password@db:5432/library_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
