8fbfc60f0eed0614b9d82536f234feaa736de885
Vlad Prokutko <147252749+Vladkees@users.noreply.github.com>
2025-05-13 21:02:21 +0300

    2

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/postgres")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
