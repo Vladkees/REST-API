b2ffa17bc0144b7eddcf8d5dc463573177bae006
Vlad Prokutko <147252749+Vladkees@users.noreply.github.com>
2025-05-13 20:20:11 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,9 @@
-from .main import app
+# app/__init__.py
+
+from fastapi import FastAPI
+from .routers import books  # Тут можеш імпортувати свої роутери
+
+def create_app():
+    app = FastAPI()
+    app.include_router(books.router)  # Підключаємо маршрути для книги
+    return app
