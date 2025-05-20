a1d5b6d9d02f377be67d949247324a1c25a543c2
Vlad Prokutko <147252749+Vladkees@users.noreply.github.com>
2025-05-20 22:43:57 +0300

    Delete app directory

--- app/models.py
+++
@@ -1,6 +0,0 @@
-from . import db
-
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), nullable=False)
-    author = db.Column(db.String(64), nullable=False)
