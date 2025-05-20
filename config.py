56ffcfd030fc0e12a4bc24d44b674d8c5325b3c0
Vlad Prokutko <147252749+Vladkees@users.noreply.github.com>
2025-05-20 22:44:19 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/postgres")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
